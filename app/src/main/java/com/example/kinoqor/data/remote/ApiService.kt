1517c6d0d04b88fb124106d78df622f6505fcbf7
aqzsha <hepokz008@gmail.com>
2025-12-05 01:20:04 +0500

    feature/K-4: done auth

--- app/src/main/java/com/example/kinoqor/data/remote/ApiService.kt
+++ app/src/main/java/com/example/kinoqor/data/remote/ApiService.kt
@@ -1,8 +1,6 @@
 package com.example.kinoqor.data.remote
 
-
-import com.example.kinoqor.data.remote.dto.LoginRequest
-import com.example.kinoqor.data.remote.dto.LoginResponse
+import com.example.kinoqor.data.remote.dto.*
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -13,4 +11,25 @@ interface ApiService {
     suspend fun login(
         @Body body: LoginRequest
     ): Response<LoginResponse>
+
+    @POST("user/create")
+    suspend fun register(
+        @Body body: RegisterRequest
+    ): Response<LoginResponse>
+
+    @POST("user/password/forgot")
+    suspend fun forgotPassword(
+        @Body body: ForgotRequest
+    ): Response<Void>
+
+    @POST("user/password/verify-pin")
+    suspend fun verifyPin(
+        @Body body: VerifyPinRequest
+    ): Response<VerifyPinResponse>
+
+    @POST("user/password/reset")
+    suspend fun resetPassword(
+        @Body body: ResetPasswordRequest
+    ): Response<Void>
+
 }
